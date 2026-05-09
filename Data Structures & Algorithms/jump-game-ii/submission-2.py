546cb11089d948a74cedf442e89bd990cf4bb5b9
Vishal Kanala <150105698+visu2211@users.noreply.github.com>
2026-05-08 20:25:47 -0700

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        choose the largest number every time and see where we go from there
+        maintain two pointers l, r
+
+        l, r represent the range of reachable indices
+        """
+        l, r = 0, 0
+        steps = 0
+        farthest = 0
+
+        while r < len(nums) - 1:
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            steps += 1
+        return steps
